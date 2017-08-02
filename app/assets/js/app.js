6dad6f88cde6c89388ad56f576ecb8a972f5c7b1
Leonardo <leo@leovidal.es>
2017-08-02 16:59:46 +0200

    sidebar hide/show problem fixed on firefox with margin-left

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -4391,6 +4391,144 @@ function calculateDailyWork( dayTypeMilliseconds, imputeType, imputeValue  ) {
     'use strict';
     angular
         .module( 'hours.projects' )
+        .controller( 'ModalDemarcate', ModalDemarcate );
+
+    ModalDemarcate.$invoke = [ '$scope', '$uibModalInstance', 'data', 'ProjectsFactory', '$rootScope', '$filter' ];
+    function ModalDemarcate( $scope, $uibModalInstance, data, ProjectsFactory, $rootScope, $filter ) {
+
+        $scope.data = data;
+
+        $scope.demarcate = function () {
+            console.log('epa');
+            $uibModalInstance.dismiss( 'cancel' );
+            ProjectsFactory.demarcateUserProject( data )
+                .then( function ( result ) {
+                    // once relationship was erased from ProjectUsers entity
+                    // we proceed to refresh the projects or users list view calling to ActiveThisUser or ActiveThisProject
+                    if( $scope.data.currentMode.type == 'users' ) {
+                        $rootScope.$broadcast( 'refreshactiveThisUser', { data : null } );
+                    } else {
+                        $rootScope.$broadcast( 'refreshactiveThisProject', { data : null } ); 
+                    };
+                    $rootScope.$broadcast( 'showThisAlertPlease', { type : 'ok', msg : $filter( 'i18next' )( 'projects.projectAssign.okDemarcate' ) } );
+                })
+                .catch( function ( err ) {
+                    $rootScope.$broadcast( 'showThisAlertPlease', { type : 'error', msg : $filter( 'i18next' )( 'projects.projectAssign.errorDemarcate' ) } );
+                });
+        };
+
+        $scope.cancel = function () {
+            $uibModalInstance.dismiss( 'cancel' );
+        };
+
+    }
+
+}());
+
+;( function () {
+    'use strict';
+    angular
+        .module( 'hours.projects' )
+        .controller( 'ModalInfo', ModalInfo );
+
+    ModalInfo.$invoke = [ '$scope', '$uibModalInstance', 'data' ];
+    function ModalInfo( $scope, $uibModalInstance, data ) {
+
+        $scope.data = data;
+
+        $scope.cancel = function () {
+            $uibModalInstance.dismiss( 'cancel' );
+        };
+
+    }
+
+}());
+
+;( function () {
+    'use strict';
+    angular
+        .module( 'hours.projects' )
+        .controller( 'ModalMarcate', ModalMarcate );
+
+    ModalMarcate.$invoke = [ '$scope', '$rootScope', '$uibModalInstance', 'data', 'ProjectsFactory', 'EmployeeManagerFactory', '$filter' ];
+    function ModalMarcate( $scope, $rootScope, $uibModalInstance, data, ProjectsFactory, EmployeeManagerFactory, $filter ) {
+
+        $scope.data = {
+                spinners    : false,
+                maxHours    : 0,
+                searchText  : '',
+                currentMode : data.currentMode
+        };
+
+        $scope.search = function( searchText ) {
+            $scope.data.spinners = true;
+            var thisPromise      = [];
+            if( $scope.data.currentMode.type == 'users' ) {
+                thisPromise.push( ProjectsFactory.advancedProjectSearch( searchText ) );
+            } else {
+                thisPromise.push( EmployeeManagerFactory.advancedUserSearch( { textToFind : searchText } ) );
+            }
+            Promise.all( thisPromise )
+                .then( function ( data ) {
+                    if( $scope.data.currentMode.type == 'users' ) {
+                        $scope.data.items = data[0].projects;
+                    } else {
+                        $scope.data.items = data[0];
+                    }
+                })
+                .catch( function ( err ) {
+                    $rootScope.$broadcast( 'showThisAlertPlease', { type : 'error', msg : $filter( 'i18next' )( 'projects.projectAssign.errorData' ) } );
+                })
+                .then( function() { // functionallity as finally
+                    $scope.data.spinners = false;
+                });
+        };
+
+        $scope.marcateThis = function( item ) {
+            var userId    = '';
+            var projectId = '';
+            if( $scope.data.currentMode.type == 'users' ) {
+                userId    = $scope.data.currentMode.obj._id;
+                projectId = item._id;
+            } else {
+                projectId = $scope.data.currentMode.obj._id;
+                userId    = item._id;
+            }
+            var data = {
+                projectId : projectId,
+                userId    : userId,
+                maxHours  : $scope.data.maxHours
+            };
+            ProjectsFactory.marcateUserProject( data )
+                .then( function ( result ) {
+                    // once the new ProjectUser document was inserted, we proceed to delete that item from list
+                    ProjectsFactory.removeItemFromArray( $scope.data.items, item._id );
+                    $rootScope.$broadcast( 'showThisAlertPlease', { type : 'ok', msg : $filter( 'i18next' )( 'projects.projectAssign.okMarcate' ) } );    
+                })
+                .catch( function ( err ) {
+                    $uibModalInstance.dismiss( 'cancel' );
+                    $rootScope.$broadcast( 'showThisAlertPlease', { type : 'error', msg : $filter( 'i18next' )( 'projects.projectAssign.errorMarcate' ) } );
+                });
+        };
+
+        $scope.cancel = function () {
+            $uibModalInstance.dismiss( 'cancel' );
+            // at modal exit, we proceed to refresh the projects or users list view calling to ActiveThisUser or ActiveThisProject
+            if( $scope.data.currentMode.type == 'users' ) {
+                $scope.$emit( 'refreshactiveThisUser', { data : null } );
+            } else {
+                $scope.$emit( 'refreshactiveThisProject', { data : null } );
+            }
+        };
+
+    }
+
+}());
+
+;( function () {
+    'use strict';
+    angular
+        .module( 'hours.projects' )
         .controller( 'ProjectAssignProjectController', ProjectAssignProjectController );
 
     ProjectAssignProjectController.$invoke = [ '$scope', 'ProjectsFactory', '$uibModal', '$rootScope', '$filter', '$timeout' ];
@@ -4536,144 +4674,6 @@ function calculateDailyWork( dayTypeMilliseconds, imputeType, imputeValue  ) {
             $scope.activeThisUser( $scope.$parent.$parent.currentMode.obj ); // to refresh projects items list view
             ProjectsFactory.setItemsOcurrences( $scope.employees ); // get and set number of ocurrences on ProjectUser entity
         });
-
-    }
-
-}());
-
-;( function () {
-    'use strict';
-    angular
-        .module( 'hours.projects' )
-        .controller( 'ModalDemarcate', ModalDemarcate );
-
-    ModalDemarcate.$invoke = [ '$scope', '$uibModalInstance', 'data', 'ProjectsFactory', '$rootScope', '$filter' ];
-    function ModalDemarcate( $scope, $uibModalInstance, data, ProjectsFactory, $rootScope, $filter ) {
-
-        $scope.data = data;
-
-        $scope.demarcate = function () {
-            console.log('epa');
-            $uibModalInstance.dismiss( 'cancel' );
-            ProjectsFactory.demarcateUserProject( data )
-                .then( function ( result ) {
-                    // once relationship was erased from ProjectUsers entity
-                    // we proceed to refresh the projects or users list view calling to ActiveThisUser or ActiveThisProject
-                    if( $scope.data.currentMode.type == 'users' ) {
-                        $rootScope.$broadcast( 'refreshactiveThisUser', { data : null } );
-                    } else {
-                        $rootScope.$broadcast( 'refreshactiveThisProject', { data : null } ); 
-                    };
-                    $rootScope.$broadcast( 'showThisAlertPlease', { type : 'ok', msg : $filter( 'i18next' )( 'projects.projectAssign.okDemarcate' ) } );
-                })
-                .catch( function ( err ) {
-                    $rootScope.$broadcast( 'showThisAlertPlease', { type : 'error', msg : $filter( 'i18next' )( 'projects.projectAssign.errorDemarcate' ) } );
-                });
-        };
-
-        $scope.cancel = function () {
-            $uibModalInstance.dismiss( 'cancel' );
-        };
-
-    }
-
-}());
-
-;( function () {
-    'use strict';
-    angular
-        .module( 'hours.projects' )
-        .controller( 'ModalInfo', ModalInfo );
-
-    ModalInfo.$invoke = [ '$scope', '$uibModalInstance', 'data' ];
-    function ModalInfo( $scope, $uibModalInstance, data ) {
-
-        $scope.data = data;
-
-        $scope.cancel = function () {
-            $uibModalInstance.dismiss( 'cancel' );
-        };
-
-    }
-
-}());
-
-;( function () {
-    'use strict';
-    angular
-        .module( 'hours.projects' )
-        .controller( 'ModalMarcate', ModalMarcate );
-
-    ModalMarcate.$invoke = [ '$scope', '$rootScope', '$uibModalInstance', 'data', 'ProjectsFactory', 'EmployeeManagerFactory', '$filter' ];
-    function ModalMarcate( $scope, $rootScope, $uibModalInstance, data, ProjectsFactory, EmployeeManagerFactory, $filter ) {
-
-        $scope.data = {
-                spinners    : false,
-                maxHours    : 0,
-                searchText  : '',
-                currentMode : data.currentMode
-        };
-
-        $scope.search = function( searchText ) {
-            $scope.data.spinners = true;
-            var thisPromise      = [];
-            if( $scope.data.currentMode.type == 'users' ) {
-                thisPromise.push( ProjectsFactory.advancedProjectSearch( searchText ) );
-            } else {
-                thisPromise.push( EmployeeManagerFactory.advancedUserSearch( { textToFind : searchText } ) );
-            }
-            Promise.all( thisPromise )
-                .then( function ( data ) {
-                    if( $scope.data.currentMode.type == 'users' ) {
-                        $scope.data.items = data[0].projects;
-                    } else {
-                        $scope.data.items = data[0];
-                    }
-                })
-                .catch( function ( err ) {
-                    $rootScope.$broadcast( 'showThisAlertPlease', { type : 'error', msg : $filter( 'i18next' )( 'projects.projectAssign.errorData' ) } );
-                })
-                .then( function() { // functionallity as finally
-                    $scope.data.spinners = false;
-                });
-        };
-
-        $scope.marcateThis = function( item ) {
-            var userId    = '';
-            var projectId = '';
-            if( $scope.data.currentMode.type == 'users' ) {
-                userId    = $scope.data.currentMode.obj._id;
-                projectId = item._id;
-            } else {
-                projectId = $scope.data.currentMode.obj._id;
-                userId    = item._id;
-            }
-            var data = {
-                projectId : projectId,
-                userId    : userId,
-                maxHours  : $scope.data.maxHours
-            };
-            ProjectsFactory.marcateUserProject( data )
-                .then( function ( result ) {
-                    // once the new ProjectUser document was inserted, we proceed to delete that item from list
-                    ProjectsFactory.removeItemFromArray( $scope.data.items, item._id );
-                    $rootScope.$broadcast( 'showThisAlertPlease', { type : 'ok', msg : $filter( 'i18next' )( 'projects.projectAssign.okMarcate' ) } );    
-                })
-                .catch( function ( err ) {
-                    $uibModalInstance.dismiss( 'cancel' );
-                    $rootScope.$broadcast( 'showThisAlertPlease', { type : 'error', msg : $filter( 'i18next' )( 'projects.projectAssign.errorMarcate' ) } );
-                });
-        };
-
-        $scope.cancel = function () {
-            $uibModalInstance.dismiss( 'cancel' );
-            // at modal exit, we proceed to refresh the projects or users list view calling to ActiveThisUser or ActiveThisProject
-            if( $scope.data.currentMode.type == 'users' ) {
-                $scope.$emit( 'refreshactiveThisUser', { data : null } );
-            } else {
-                $scope.$emit( 'refreshactiveThisProject', { data : null } );
-            }
-        };
 
     }
 
